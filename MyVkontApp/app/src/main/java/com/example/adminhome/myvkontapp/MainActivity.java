08ef2d4a5530ad485d5a696a55e3a926bc162981
Capitalsav <capitalsav@gmail.com>
2016-07-06 13:39:23 +0300

    Added button "Show message". When button was clicked - it will show to you your 10 latest messages in listView

--- MyVkontApp/app/src/main/java/com/example/adminhome/myvkontapp/MainActivity.java
+++ MyVkontApp/app/src/main/java/com/example/adminhome/myvkontapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.adminhome.myvkontapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,15 +19,19 @@ import com.vk.sdk.api.VKError;
 import com.vk.sdk.api.VKParameters;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiGetMessagesResponse;
+import com.vk.sdk.api.model.VKApiMessage;
 import com.vk.sdk.api.model.VKList;
 import com.vk.sdk.util.VKUtil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] scope = new String[] {VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
     private ListView listView;
+    private Button showMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VKSdk.login(this, scope);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        showMessage = (Button) findViewById(R.id.showMessage);
+        showMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                VKRequest request = VKApi.messages().get(VKParameters.from(VKApiConst.COUNT,
+                        10));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKApiGetMessagesResponse getMessagesResponse = (
+                                VKApiGetMessagesResponse) response.parsedModel;
+
+                        VKList<VKApiMessage> list = getMessagesResponse.items;
+
+                        ArrayList<String> arrayList = new ArrayList<String>();
+
+                        for (VKApiMessage msg : list) {
+                            arrayList.add(msg.body);
+                        }
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                                MainActivity.this, android.R.layout.simple_expandable_list_item_1,
+                                arrayList);
+
+                        listView.setAdapter(arrayAdapter);
+                    }
+                });
+            }
+
+        });
+
+
     }
 
     @Override
@@ -40,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
-                listView = (ListView) findViewById(R.id.listView);
 
                 VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS,
                         "first_name, last_name"));
