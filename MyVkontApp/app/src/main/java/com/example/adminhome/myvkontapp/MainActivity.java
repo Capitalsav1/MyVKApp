bbffc4452ae8bf542f3301ca7c55d2d61f234e22
Capitalsav <capitalsav@gmail.com>
2016-07-06 09:21:29 +0300

    Added authorization to app

--- MyVkontApp/app/src/main/java/com/example/adminhome/myvkontapp/MainActivity.java
+++ MyVkontApp/app/src/main/java/com/example/adminhome/myvkontapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.adminhome.myvkontapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] scope = new String[] {VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VKSdk.login(this, scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast.makeText(getApplicationContext(), "Good", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
